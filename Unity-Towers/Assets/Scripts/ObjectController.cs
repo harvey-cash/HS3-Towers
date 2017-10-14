967afdbbfea041393d08b6264d15ad951035ec43
HOCgaming <harveycash@outlook.com>
2017-10-14 14:45:39 +0100

    Fixed some silly mistakes, starting object vis.

--- Unity-Towers/Assets/Scripts/ObjectController.cs
+++ Unity-Towers/Assets/Scripts/ObjectController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectController : MonoBehaviour {
+public class ObjectController {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private BoardState boardState;
+	public ObjectController(BoardState board) {
+        this.boardState = board;
+    }
+
+    /* Create the physical board */
+    private List<GameObject> grids = new List<GameObject>();
+    public void DrawGrids() {
+        foreach(GameObject thing in grids) {
+            Object.Destroy(thing);
+        }
+        grids = new List<GameObject>();
+
+        for(int x = 0; x < boardState.GetBoard().GetLength(0); x++) {
+            for (int y = 0; y < boardState.GetBoard().GetLength(1); y++) {
+                Grid grid = boardState.GetBoard()[x, y];
+                GameObject thing = (GameObject)Object.Instantiate(Resources.Load("Grid"));                
+
+                thing.transform.position = new Vector3(grid.GetCoords().x, -1, grid.GetCoords().y);
+                grids.Add(thing);
+            }
+        }
+    }
 }
--- Unity-Towers/Assets/Scripts/Objects/BoardState.cs
+++ Unity-Towers/Assets/Scripts/Objects/BoardState.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoardState : MonoBehaviour {
+public class BoardState {
 
-    private Grid[,] board;    
+    private Grid[,] board; 
+    public Grid[,] GetBoard() { return board; }   
+
     private Player.TEAM turnOf;
 
     public BoardState(Grid[,] board, Player.TEAM turnOf) {
         this.board = board;
         this.turnOf = turnOf;
+    }
+
+    public override string ToString() {
+        return board.ToString() + " currently turn of: " + turnOf.ToString();
     }
 }
